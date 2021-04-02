2a26ecafa5281c82240a94d7835c65cec9c314bd
elephantcastle <obymik89@gmail.com>
2021-04-02 14:15:56 +0200

    store chart options in dedicated file

--- pages/results.vue
+++ pages/results.vue
@@ -26,52 +26,20 @@
 
 <script>
 import { mapState } from "vuex";
+import config from "./../utils/chart-options"
 
 export default {
   name: "results",
   data() {
     return {
       options: {
-        chart: {
-          width: "75%",
-          height: "100%",
-          type: "bar",
-        },
-        legend: {
-          show: false,
-        },
-        plotOptions: {
-          bar: {
-            distributed: true,
-          },
-        },
-        xaxis: {},
-        yaxis: {
-          min: 0,
-          max: 100
-        },
-        fill: {
-          colors: ["#008FFB", "#00E396", "#FEB019", "#FF4560", "#775DD0"],
-        },
-        dataLabels: {
-          style: {
-            colors: ["#5A2A27"],
-          },
-        },
-        responsive: [
-          {
-            breakpoint: 750,
-            options: {
-              chart: {
-                height: 450,
-              },
-            },
-          },
-        ],
+        ...config
       },
     };
   },
-  created() {},
+  created() {
+    console.log(config)
+  },
   computed: {
     ...mapState(["graphData"]),
     traitOptions() {
