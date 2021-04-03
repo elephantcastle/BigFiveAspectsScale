205751b4241f481a5faf68e0b2b5df13c6031b92
elephantcastle <obymik89@gmail.com>
2021-04-03 12:42:07 +0200

    add loader when questions end

--- pages/test.vue
+++ pages/test.vue
@@ -3,19 +3,28 @@
     <v-progress-linear :value="progress" height="25" rounded color="light-blue">
       <strong>{{ Math.ceil(progress) }}%</strong>
     </v-progress-linear>
-    <v-card class="question">
-      {{ $t("questions")[index] }}
-    </v-card>
-    <div class="answers-container">
-      <v-card
-        class="answers"
-        v-for="(answer, i) in $t('answers')"
-        :key="i"
-        @click="setAnswer(i)"
-      >
-        {{ answer }}
+    <div v-if="index<100">
+      <v-card class="question">
+        {{ $t("questions")[index] }}
       </v-card>
+      <div class="answers-container">
+        <v-card
+          class="answers"
+          v-for="(answer, i) in $t('answers')"
+          :key="i"
+          @click="setAnswer(i)"
+        >
+          {{ answer }}
+        </v-card>
+      </div>
     </div>
+    <v-progress-circular
+      class="loader"
+      v-else
+      :size="90"
+      indeterminate
+      color="primary"
+    ></v-progress-circular>
   </v-container>
 </template>
 
@@ -27,7 +36,7 @@ export default {
   name: "test",
   data() {
     return {
-      index: 0,
+      index: 0
     };
   },
   computed: {
@@ -39,34 +48,35 @@ export default {
   methods: {
     ...mapMutations(["updateAnswer", "calculateResults"]),
     setAnswer(value) {
-      if (this.index === 99) {
-
-        const payload = {};
-        payload["testdata"] = JSON.parse(JSON.stringify(this.results));
-        payload["sex"] = this.sex;
-        payload["age"] = this.age;
-        payload["language"] = this.$i18n.locale;
-
-        const API =
-          process.env.NODE_ENV === "production"
-            ? "https://bigfiveaspectsapi.herokuapp.com"
-            : "http://localhost:4000";
-
-        axios
-          .post(`${API}/results`, payload)
-          .then((response) => {
-            this.calculateResults();
-            this.$router.push({ path: `/results` });
-          })
-          .catch((e) => {
-            console.log(e);
-            this.errors.push(e);
-            this.$router.push("/crashpage");
-          });
-
-      } else {
+      if(this.index<100){
         this.updateAnswer([this.index, value + 1]);
         this.index++;
+        if (this.index === 100) {
+          this.processing = true;
+
+          const payload = {};
+          payload["testdata"] = JSON.parse(JSON.stringify(this.results));
+          payload["sex"] = this.sex;
+          payload["age"] = this.age;
+          payload["language"] = this.$i18n.locale;
+  
+          const API =
+            process.env.NODE_ENV === "production"
+              ? "https://bigfiveaspectsapi.herokuapp.com"
+              : "http://localhost:4000";
+
+          axios
+            .post(`${API}/results`, payload)
+            .then((response) => {
+              this.calculateResults();
+              this.$router.push({ path: `/results` });
+            })
+            .catch((e) => {
+              console.log(e);
+              this.errors.push(e);
+              this.$router.push("/crashpage");
+            });
+        }
       }
     },
   },
@@ -79,6 +89,11 @@ export default {
   padding: 20px;
   font-size: 30px;
   border-radius: 10px;
+}
+.loader{
+  position: absolute;
+  top: 50%;
+  left: calc(50% - 41px);
 }
 .answers {
   margin: 20px;
